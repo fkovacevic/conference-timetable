c14be082d2267e73d02c1510f70ae228824a3bec
Pilip8 <filip.kovacevic@fer.hr>
2021-11-04 18:52:38 +0100

    Minor changes to make the manifest work and to activate service worker properly

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,10 +1,17 @@
 /* eslint-disable no-restricted-globals */
 
+// This service worker can be customized!
+// See https://developers.google.com/web/tools/workbox/modules
+// for the list of available Workbox modules, or add any other
+// code you'd like.
+// You can also remove this file if you'd prefer not to use a
+// service worker, and the Workbox build step will be skipped.
+
 import { clientsClaim } from 'workbox-core';
 import { ExpirationPlugin } from 'workbox-expiration';
 import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
-import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
+import { StaleWhileRevalidate } from 'workbox-strategies';
 
 clientsClaim();
 
@@ -54,14 +61,6 @@ registerRoute(
 	})
 );
 
-
-// Most basic example of caching
-registerRoute(
-	'https://random-data-api.com/api/users/random_user',
-	new CacheFirst({
-		cacheName: 'randomUser',
-	})
-);
 
 // This allows the web app to trigger skipWaiting via
 // registration.waiting.postMessage({type: 'SKIP_WAITING'})
